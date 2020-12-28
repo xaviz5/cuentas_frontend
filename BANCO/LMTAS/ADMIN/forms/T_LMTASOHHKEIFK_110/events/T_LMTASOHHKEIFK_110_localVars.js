28dc01f714fca3273627896e537d478c77c73652
xaviz5 <xavier5-20_lenin@hotmail.com>
2020-12-28 17:46:07 -0500

    Consulta cliente termianda

---
+++ BANCO/LMTAS/ADMIN/forms/T_LMTASOHHKEIFK_110/events/T_LMTASOHHKEIFK_110_localVars.js
@@ -0,0 +1,1 @@
+/* variables locales de T_LMTASOHHKEIFK_110*/
--- BANCO/LMTAS/ADMIN/forms/T_LMTASOHHKEIFK_110/events/executeQuery/Q_CLIEETTE_RL85.js
+++ BANCO/LMTAS/ADMIN/forms/T_LMTASOHHKEIFK_110/events/executeQuery/Q_CLIEETTE_RL85.js
@@ -1,6 +1,7 @@
 
 //ClienteQuery Entity: Cliente
 task.executeQuery.Q_CLIEETTE_RL85 = function(executeQueryEventArgs){
+    executeQueryEventArgs.parameters.estado=executeQueryEventArgs.commons.api.vc.model.Filtro.numCed;
     executeQueryEventArgs.commons.execServer = true;
     //executeQueryEventArgs.commons.serverParameters.Cliente = true;
 };
