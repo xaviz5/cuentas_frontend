507b8feb949a675d7e3a75338a00937b2d196c54
xaviz5 <xavier5-20_lenin@hotmail.com>
2020-12-28 15:07:59 -0500

    Cambio de modulo y creacion de formulario para consultar cliente

---
+++ BANCO/LMTAS/ADMIN/forms/T_LMTASOHHKEIFK_110/events/executeQuery/Q_CLIEETTE_RL85.js
@@ -0,0 +1,6 @@
+
+//ClienteQuery Entity: Cliente
+task.executeQuery.Q_CLIEETTE_RL85 = function(executeQueryEventArgs){
+    executeQueryEventArgs.commons.execServer = true;
+    //executeQueryEventArgs.commons.serverParameters.Cliente = true;
+};
